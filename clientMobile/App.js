357f5df7fedfc8ac82ab7f69174edc5b14d939a4
AsadAnik <s.engineer63@gmail.com>
2022-02-07 01:32:37 +0600

    React Native Navigation added

--- clientMobile/App.js
+++ clientMobile/App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { LogBox } from 'react-native';
 
+// use of this LogBox..
+// to skip warning of 'react-native-gesture-handler' updates..
+LogBox.ignoreLogs([
+    "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+]);
+
+// Screens..
+import HomeScreen from './screens/Home';
+import ProfileScreen from './screens/Profile';
+
+// the Stack navigator..
+const BottomTab = createBottomTabNavigator();
+
+// App..
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <BottomTab.Navigator>
+        {/* ------ Screen of Home ----- */}
+        <BottomTab.Screen
+          name="Home"
+          component={ HomeScreen }
+          options={{ title: 'Home Screen' }}
+        />
+
+        {/* ------ Screen of Profile ----- */}
+        <BottomTab.Screen
+          name="Profile"
+          component={ ProfileScreen }
+          options={{ title: 'Profile Screen' }}
+        />
+      </BottomTab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
