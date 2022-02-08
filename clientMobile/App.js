05928a264cdfc91d8a433b22520989c5fe3724e7
AsadAnik <s.engineer63@gmail.com>
2022-02-09 01:36:55 +0600

    Bottom Tab Navigation of iOS done

--- clientMobile/App.js
+++ clientMobile/App.js
@@ -3,6 +3,9 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { LogBox } from 'react-native';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import FontistoIcon from 'react-native-vector-icons/Fontisto';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
 
 // use of this LogBox..
 // to skip warning of 'react-native-gesture-handler' updates..
@@ -13,29 +16,90 @@ LogBox.ignoreLogs([
 // Screens..
 import HomeScreen from './screens/Home';
 import ProfileScreen from './screens/Profile';
+import AddPostScreen from './screens/AddPost';
+import NotificationScreen from './screens/Notification';
+import SettingsScreen from './screens/Settings';
 
 // the Stack navigator..
 const BottomTab = createBottomTabNavigator();
 
 // App..
-export default function App() {
+const App = () => {
   return (
-    <NavigationContainer>
-      <BottomTab.Navigator>
-        {/* ------ Screen of Home ----- */}
-        <BottomTab.Screen
-          name="Home"
-          component={ HomeScreen }
-          options={{ title: 'Home Screen' }}
-        />
+      <NavigationContainer>
+          <BottomTab.Navigator
+            initialRouteName="Home"
+            activeColor="red"
+            barStyle={{backgroundColor: 'tomato'}}
+          >
+              {/* ------ Screen of Home ----- */}
+              <BottomTab.Screen
+                  name="Home"
+                  component={ HomeScreen }
+                  options={{
+                      title: 'Home Screen',
+                      tabBarLabel: 'HOME',
+                      tabBarIcon: ({ color }) => (
+                          <FontAwesomeIcon name="home" size={30} color={color} />
+                      ),
+                  }}
+              />
 
-        {/* ------ Screen of Profile ----- */}
-        <BottomTab.Screen
-          name="Profile"
-          component={ ProfileScreen }
-          options={{ title: 'Profile Screen' }}
-        />
-      </BottomTab.Navigator>
-    </NavigationContainer>
+              {/* ------ Screen of Notification ----- */}
+              <BottomTab.Screen
+                  name="Notification"
+                  component={ NotificationScreen }
+                  options={{
+                      title: 'Notification Screen',
+                      tabBarLabel: 'NOTIFICATION',
+                      tabBarIcon: ({ color }) => (
+                          <EntypoIcon name="notification" size={30} color={color} />
+                      ),
+                      tabBarBadge: 30
+                  }}
+              />
+
+              {/* ------ Screen of AddPost ----- */}
+              <BottomTab.Screen
+                  name="Add Post"
+                  component={ AddPostScreen }
+                  options={{
+                      title: 'Add Post Screen',
+                      tabBarLabel: 'ADD',
+                      tabBarIcon: ({ color }) => (
+                          <EntypoIcon name="circle-with-plus" size={30} color={color} />
+                      ),
+                  }}
+              />
+
+              {/* ------ Screen of Profile ----- */}
+              <BottomTab.Screen
+                  name="Profile"
+                  component={ ProfileScreen }
+                  options={{
+                      title: 'Profile Screen',
+                      tabBarLabel: 'PROFILE',
+                      tabBarIcon: ({ color }) => (
+                        <FontistoIcon name="person" size={30} color={color} />
+                      ),
+                  }}
+              />
+
+              {/* ------ Screen of Settings ----- */}
+              <BottomTab.Screen
+                name="Settings"
+                component={ SettingsScreen }
+                options={{
+                    title: 'Settings Screen',
+                    tabBarLabel: 'SETTINGS',
+                    tabBarIcon: ({ color }) => (
+                        <FontistoIcon name="player-settings" size={30} color={color} />
+                    ),
+                }}
+              />
+          </BottomTab.Navigator>
+      </NavigationContainer>
   );
 }
+
+export default App;
---
+++ clientMobile/screens/AddPost.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const AddPost = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Add Post!</Text>
+        </View>
+    );
+};
+
+export default AddPost;
