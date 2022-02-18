30c2cf775b11e6d07c23f71b5e180b1e11e284e7
AsadAnik <s.engineer63@gmail.com>
2022-02-18 23:16:20 +0600

    Styled for bithdate card on profile

--- client/src/components/commons/Birthdate.js
+++ client/src/components/commons/Birthdate.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import { Paper } from '@mui/material';
 import StylesModule from '../../css/birthdate.module.css';
+import CakeIcon from '@mui/icons-material/Cake';
+
+// CSS Style for birthdate icon..
+const commonThemeColor = 'royalblue';
+const birthdateIcon = {
+    fontSize: '30px',
+    position: 'relative',
+    top: '5px',
+    color: commonThemeColor
+};
+
+// CSS Style for birthdate text..
+const birthdateText = {
+    background: commonThemeColor,
+    color: 'white',
+    borderRadius: '5px',
+    textAlign: 'center'
+};
+
 
 // Component of Birthdate..
 const Birthdate = () => {
     return (
         <Paper className={StylesModule.birthdatePaper}>
-            <h3>Thu Jan 13 2000 19:55:41 GMT+0600 (+06)</h3>
+            <h3>
+                <span><CakeIcon style={birthdateIcon} /></span>
+                <span> </span>
+                <span>Birthdate</span>
+            </h3>
+
+            <p style={birthdateText}>Thu Jan 13 2000 19:55:41 GMT+0600 (+06)</p>
         </Paper>
     );
 };
