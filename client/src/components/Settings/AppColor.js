37a413944a6daa9be35aa0ea3e4622990cff9528
AsadAnik <s.engineer63@gmail.com>
2022-04-03 16:23:54 +0600

    AppColor's FrontEnd Chapter Designed as well

--- client/src/components/Settings/AppColor.js
+++ client/src/components/Settings/AppColor.js
@@ -1,54 +1,10 @@
 import React from 'react';
 import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import { LightMode as LightModeIcon, DarkMode as DarkModeIcon, Contrast as ContrastIcon } from '@mui/icons-material';
 import CircleIcon from '@mui/icons-material/Circle';
 import StylesModule from '../../css/settings.module.css';
 
-const AppColor = () => {
-    // const [appColor, setAppColor] = React.useState({
-    //     lightgray: false,
-    //     royalblue: false,
-    //     green: false,
-    //     red: false,
-    // });
-
-    const [lightgray, setLightgray] = React.useState(false);
-    const [royalblue, setRoyalblue] = React.useState(false);
-    const [green, setGreen] = React.useState(false);
-    const [red, setRed] = React.useState(false);
-
-    const makeActiveColor = (type) => {
-        switch (type) {
-            case "lightgray":
-                setLightgray(true);
-                setRoyalblue(false);
-                setGreen(false);
-                setRed(false);
-                break;
-
-            case "royalblue":
-                setLightgray(false);
-                setRoyalblue(true);
-                setGreen(false);
-                setRed(false);
-                break;
-
-            case "green":
-                setLightgray(false);
-                setRoyalblue(false);
-                setGreen(true);
-                setRed(false);
-                break;
-
-            case "red":
-                setLightgray(false);
-                setRoyalblue(false);
-                setGreen(false);
-                setRed(true);
-                break;
-        }
-    };
-
+// Main component..
+const AppColor = ({ colorMode, makeChangeAppColor }) => {
     return (
         <List>
             <ListItem>
@@ -57,24 +13,25 @@ const AppColor = () => {
 
             {/*-------- App Color's --------*/}
             <ListItem>
-                <ListItemIcon onClick={() => makeActiveColor('lightgray')}>
-                    <CircleIcon className={StylesModule.modeIcon} style={{ color: 'lightgray', background: `${lightgray && 'lightgrey'}` }} />
+                <ListItemIcon onClick={() => makeChangeAppColor('royalblue')}>
+                    <CircleIcon className={StylesModule.modeIcon} style={{ color: 'royalblue', background: `${colorMode.royalblue && 'lightgrey'}` }} />
                 </ListItemIcon>
 
-                <ListItemIcon onClick={() => makeActiveColor('royalblue')}>
-                    <CircleIcon className={StylesModule.modeIcon} style={{ color: 'royalblue', background: `${royalblue && 'lightgrey'}` }} />
+                <ListItemIcon onClick={() => makeChangeAppColor('black')}>
+                    <CircleIcon className={StylesModule.modeIcon} style={{ color: 'black', background: `${colorMode.black && 'lightgrey'}` }} />
                 </ListItemIcon>
 
-                <ListItemIcon onClick={() => makeActiveColor('green')}>
-                    <CircleIcon className={StylesModule.modeIcon} style={{ color: 'green', background: `${green && 'lightgrey'}` }} />
+                <ListItemIcon onClick={() => makeChangeAppColor('green')}>
+                    <CircleIcon className={StylesModule.modeIcon} style={{ color: 'green', background: `${colorMode.green && 'lightgrey'}` }} />
                 </ListItemIcon>
 
-                <ListItemIcon onClick={() => makeActiveColor('red')}>
-                    <CircleIcon className={StylesModule.modeIcon} style={{ color: 'red', background: `${red && 'lightgrey'}` }} />
+                <ListItemIcon onClick={() => makeChangeAppColor('red')}>
+                    <CircleIcon className={StylesModule.modeIcon} style={{ color: 'red', background: `${colorMode.red && 'lightgrey'}` }} />
                 </ListItemIcon>
             </ListItem>
         </List>
     );
 };
+
 
 export default AppColor;
--- client/src/components/Settings/index.js
+++ client/src/components/Settings/index.js
@@ -15,77 +15,125 @@ import {
 } from '@mui/icons-material';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
-import { themeModeLight, themeModeDark, themeModeHighContrast } from '../../redux/actions/SettingActions';
+import { 
+    themeModeLight, 
+    themeModeDark, 
+    themeModeHighContrast,
+    appColorBlack,
+    appColorRoyalblue,
+    appColorGreen,
+    appColorRed
+} from '../../redux/actions/SettingActions';
 import ThemeMode from './ThemeMode';
 import AppColor from './AppColor';
+
 
 // Main Settings Component..
 const Settings = (props) => {
     const [drawerOpen, setDrawerOpen] = useState(false);
-    const [lightMode, setLightMode] = useState(true);
-    const [darkMode, setDarkMode] = useState(false);
-    const [contrastMode, setContrastMode] = useState(false);
+    const [themeModes, setThemeModes] = useState({
+        lightMode: false,
+        darkMode: false,
+        contrastMode: false
+    });
+    const [appColor, setAppColor] = useState({
+        royalblue: false,
+        black: false,
+        red: false,
+        green: false
+    });
 
-    // const initialTheme = {
-    //     themeMode: {
-    //         backgroundColor: 'white',
-    //         textColor: 'black',
-    //         iconColor: 'pink'
-    //     }
-    // };
+    // ThemeMode Controller..
+    const themeModeController = (mode) => {
+        // working on themeMode...
+        if (mode === 'lightMode') {
+            setThemeModes({
+                lightMode: true,
+                darkMode: false,
+                contrastMode: false
+            });
+            props.dispatch(themeModeLight());
+        }
+
+        if (mode === 'darkMode') {
+            setThemeModes({
+                lightMode: false,
+                darkMode: true,
+                contrastMode: false
+            });
+            props.dispatch(themeModeDark());
+        }
+
+        if (mode === 'contrastMode') {
+            setThemeModes({
+                lightMode: false,
+                darkMode: false,
+                contrastMode: true
+            });
+            props.dispatch(themeModeHighContrast());
+        }
+    };
+
+    // AppColor Controller...
+    const colorModeController = (mode) => {
+        // working on colorMode..
+        if (mode === 'royalblue') {
+            setAppColor({ ...appColor, royalblue: true });
+            props.dispatch(appColorRoyalblue());
+        }
+
+        if (mode === 'black') {
+            setAppColor({ ...appColor, black: true });
+            props.dispatch(appColorBlack());
+        }
+
+        if (mode === 'red') {
+            setAppColor({ ...appColor, red: true });
+            props.dispatch(appColorRed());
+        }
+
+        if (mode === 'green') {
+            setAppColor({ ...appColor, green: true });
+            props.dispatch(appColorGreen());
+        }
+    };
 
     // React Hook...
     useEffect(() => {
         // useEffect.. body area..
         // const { themeMode } = props.User.login;
 
-        if (props.User){
-           if (props.User.login){
-               const { themeMode } = props.User.login;
+        if (props.User) {
+            if (props.User.login) {
+                const { themeMode, colorMode } = props.User.login;
 
-               // console.log('MODE  -->> ', themeMode);
+                // console.log('MODE  -->> ', themeMode);
 
-               if (themeMode === 'lightMode'){
-                   setLightMode(true);
-                   setDarkMode(false);
-                   setContrastMode(false);
-                   props.dispatch(themeModeLight());
-               }
+                // manage themeMode..
+                themeModeController(themeMode);
 
-               if (themeMode === 'darkMode'){
-                   setLightMode(false);
-                   setDarkMode(true);
-                   setContrastMode(false);
-                   props.dispatch(themeModeDark());
-               }
-
-               if (themeMode === 'contrastMode'){
-                   setLightMode(false);
-                   setDarkMode(false);
-                   setContrastMode(true);
-                   props.dispatch(themeModeHighContrast());
-               }
-           }
+                // manage colorMode..
+                colorModeController(colorMode);
+            }
         }
 
 
         // cleanup function...
         return () => {
-            setLightMode(false);
-            setDarkMode(false);
-            setContrastMode(false);
+            setThemeModes({ ...themeModes });
+            setAppColor({ ...appColor });
         };
     }, [props.User.login]);
 
-    // console.log('Settings ReducthmeMode.ers here -- ', props);
+    // console.log('Settings PROPS = here -- ', props);
 
     // themeMode Object..
     const themeMode = { backgroundColor: 'white', textColor: 'black', iconColor: 'gray' };
 
-    if(props.Settings){
-        if (props.Settings.themeMode){
+    if (props.Settings) {
+        if (props.Settings.themeMode) {
             // themeMode's here..
-            const {  backgroundColor, textColor, iconColor  } = props.Settings.themeMode;
+            const { backgroundColor, textColor, iconColor } = props.Settings.themeMode;
 
             themeMode.backgroundColor = backgroundColor;
             themeMode.textColor = textColor;
@@ -99,7 +147,7 @@ const Settings = (props) => {
         }
     }
 
-    console.log('Theme is now for there -- ', themeMode);
+    // console.log('Theme is now for there -- ', themeMode);
 
     // Styled Paper Component..
     // Used this Styled Component to pass CSS to Drawer's Paper props inner drawer component..
@@ -112,91 +160,149 @@ const Settings = (props) => {
 
     // toggle drawer is there..
     const toggleDrawer = (open) => (event) => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')){
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
             return;
         }
         setDrawerOpen(open);
     };
 
-    // make contrast mode..
-    const makeContrastMode = () => {
-        setLightMode(false);
-        setDarkMode(false);
-        setContrastMode(true);
-        props.dispatch(themeModeHighContrast());
+    // the function for generating themeMode..
+    const makeChangeThemeMode = (type) => {
+        switch (type) {
+            case "lightMode":
+                setThemeModes({
+                    lightMode: true,
+                    darkMode: false,
+                    contrastMode: false
+                });
+                props.dispatch(themeModeLight());
+                break;
+
+            case "darkMode":
+                setThemeModes({
+                    lightMode: false,
+                    darkMode: true,
+                    contrastMode: false
+                });
+                props.dispatch(themeModeDark());
+                break;
+
+            case "contrastMode":
+                setThemeModes({
+                    lightMode: false,
+                    darkMode: false,
+                    contrastMode: true
+                });
+                props.dispatch(themeModeHighContrast());
+                break;
+
+            default:
+                setThemeModes({ ...themeModes });
+        }
     };
 
-    // make light mode..
-    const makeLightMode = () => {
-        setLightMode(true);
-        setDarkMode(false);
-        setContrastMode(false);
-        props.dispatch(themeModeLight());
-    };
+    // the function for generating color..
+    const makeChangeAppColor = (type) => {
+        switch (type) {
+            case "black":
+                setAppColor({
+                    royalblue: false,
+                    black: true,
+                    green: false,
+                    red: false
+                });
+                props.dispatch(appColorBlack());
+                break;
 
-    // make dark mode..
-    const makeDarkMode = () => {
-        setLightMode(false)
-        setDarkMode(true);
-        setContrastMode(false);
-        props.dispatch(themeModeDark());
+            case "royalblue":
+                setAppColor({
+                    royalblue: true,
+                    black: false,
+                    green: false,
+                    red: false
+                });
+                props.dispatch(appColorRoyalblue());
+                break;
+
+            case "green":
+                setAppColor({
+                    royalblue: false,
+                    black: false,
+                    green: true,
+                    red: false
+                });
+                props.dispatch(appColorGreen());
+                break;
+
+            case "red":
+                setAppColor({
+                    royalblue: false,
+                    black: false,
+                    green: false,
+                    red: true
+                });
+                props.dispatch(appColorRed());
+                break;
+
+            default:
+                setAppColor({ ...appColor });
+        }
     };
 
     // list component..
     const list = (anchor) => (
-           <Box
-               sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 450 }}
-               role="presentation"
-               onClick={toggleDrawer(anchor, false)}
-               onKeyDown={toggleDrawer(anchor, false)}
-           >
-               <List>
-                   {/*--- This is first header here for settings and close the settings ----*/}
-                   <ListItem>
-                       <ListItemIcon style={{ color: themeMode.iconColor }}>
-                           <SettingsIcon />
-                       </ListItemIcon>
-                       <ListItemText primary={'Settings'} />
-                   </ListItem>
-               </List>
-               <Divider style={{ background: themeMode.iconColor }} />
+        <Box
+            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 450 }}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                {/*--- This is first header here for settings and close the settings ----*/}
+                <ListItem>
+                    <ListItemIcon style={{ color: themeMode.iconColor }}>
+                        <SettingsIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={'Settings'} />
+                </ListItem>
+            </List>
+            <Divider style={{ background: themeMode.iconColor }} />
 
-               {/*----- Application's Theme Mode Here -----*/}
-               <ThemeMode
-                    lightMode={lightMode}
-                    darkMode={darkMode}
-                    contrastMode={contrastMode}
-                    iconColor={themeMode.iconColor}
-                    makeLightMode={makeLightMode}
-                    makeDarkMode={makeDarkMode}
-                    makeContrastMode={makeContrastMode}
-                />
+            {/*----- Application's Theme Mode Here -----*/}
+            <ThemeMode
+                themeModes={themeModes}
+                iconColor={themeMode.iconColor}
+                makeChangeThemeMode={makeChangeThemeMode}
+            />
 
-                {/*----- Application's App Color Here -----*/}
-                <AppColor />
+            {/*----- Application's App Color Here -----*/}
+            <AppColor
+                colorMode={appColor}
+                makeChangeAppColor={makeChangeAppColor}
+            />
 
-               <List>
-                   {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                       <ListItem button key={text}>
-                           <ListItemIcon style={{ color: themeMode.iconColor }}>
-                               {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                           </ListItemIcon>
-                           <ListItemText primary={text} />
-                       </ListItem>
-                   ))}
-               </List>
-               <Divider style={{ background: themeMode.iconColor }} />
-               <List>
-                   {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                       <ListItem button key={text}>
-                           <ListItemIcon style={{ color: themeMode.iconColor }}>
-                               {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                           </ListItemIcon>
-                           <ListItemText primary={text} />
-                       </ListItem>
-                   ))}
-               </List>
-           </Box>
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon style={{ color: themeMode.iconColor }}>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider style={{ background: themeMode.iconColor }} />
+            <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon style={{ color: themeMode.iconColor }}>
+                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
     );
 
     // returning statement..
