a8cab4ce11b456876dc05b48a311f303aa44ca20
Yeaseen Arafat <yeaseenarafat@Yeaseens-MacBook-Pro.local>
2024-12-23 02:23:05 +0600

    adding guester

--- metaApp/src/screens/tabs/ChatScreen.tsx
+++ metaApp/src/screens/tabs/ChatScreen.tsx
@@ -1,5 +1,12 @@
-import React, { useRef, useCallback } from 'react';
-import { FlatList, RefreshControl, Alert, StyleSheet, Text } from 'react-native';
+import React, {useRef, useCallback} from 'react';
+import {
+  FlatList,
+  RefreshControl,
+  Alert,
+  StyleSheet,
+  Text,
+  Button,
+} from 'react-native';
 import {
   Container,
   Card,
@@ -12,8 +19,13 @@ import {
   MessageText,
   TextSection,
 } from '../../styles/MessageStyle';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import BottomSheet, {
+  BottomSheetModal,
+  BottomSheetView,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet';
+
 // import ChatTabScreen from './ChatScreen';
 
 type Message = {
@@ -30,35 +42,40 @@ const Messages: Message[] = [
     userName: 'Jenny Doe',
     userImg: require('../../assets/images/asadanik.jpg'),
     messageTime: '4 mins ago',
-    messageText: 'Hey there, this is my test for a post of my social app in React Native.',
+    messageText:
+      'Hey there, this is my test for a post of my social app in React Native.',
   },
   {
     id: '2',
     userName: 'John Doe',
     userImg: require('../../assets/images/asadanik.jpg'),
     messageTime: '2 hours ago',
-    messageText: 'Hey there, this is my test for a post of my social app in React Native.',
+    messageText:
+      'Hey there, this is my test for a post of my social app in React Native.',
   },
   {
     id: '3',
     userName: 'Ken William',
     userImg: require('../../assets/images/asadanik.jpg'),
     messageTime: '1 hours ago',
-    messageText: 'Hey there, this is my test for a post of my social app in React Native.',
+    messageText:
+      'Hey there, this is my test for a post of my social app in React Native.',
   },
   {
     id: '4',
     userName: 'Selina Paul',
     userImg: require('../../assets/images/asadanik.jpg'),
     messageTime: '1 day ago',
-    messageText: 'Hey there, this is my test for a post of my social app in React Native.',
+    messageText:
+      'Hey there, this is my test for a post of my social app in React Native.',
   },
   {
     id: '5',
     userName: 'Christy Alex',
     userImg: require('../../assets/images/asadanik.jpg'),
     messageTime: '2 days ago',
-    messageText: 'Hey there, this is my test for a post of my social app in React Native.',
+    messageText:
+      'Hey there, this is my test for a post of my social app in React Native.',
   },
 ];
 
@@ -66,41 +83,37 @@ type MessagesScreenProps = {
   navigation: any; // Replace `any` with the correct type if you're using a type-safe navigation library like React Navigation
 };
 
-const ChatTabScreen: React.FC<MessagesScreenProps> = ({ navigation }) => {
-
+const ChatTabScreen: React.FC<MessagesScreenProps> = ({navigation}) => {
   // ref
-  const bottomSheetRef = useRef<BottomSheet>(null);
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
   // callbacks
+  // const handleSheetChanges = useCallback((index: number) => {
+  //   console.log('handleSheetChanges', index);
+  // }, []);
+
+  // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
   const handleSheetChanges = useCallback((index: number) => {
     console.log('handleSheetChanges', index);
   }, []);
 
-
-
   return (
-    <GestureHandlerRootView style={styles.container}>
-
-      <BottomSheet
-        ref={bottomSheetRef}
-        onChange={handleSheetChanges}
-      >
-        <BottomSheetView style={styles.contentContainer}>
-          <Text>Awesome 🎉</Text>
-        </BottomSheetView>
-      </BottomSheet>
-
-
-      <Container>
+    <>
+      <GestureHandlerRootView>
+        <Container>
         <FlatList
           refreshControl={<RefreshControl refreshing={false} />}
           data={Messages}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
             <Card
-              onPress={() => navigation.navigate('Messages', { userName: item.userName })}
-              onLongPress={() => Alert.alert('Long Press')}
-            >
+              onPress={() =>
+                navigation.navigate('Messages', {userName: item.userName})
+              }
+              onLongPress={handlePresentModalPress}>
               <UserInfo>
                 <UserImgWrapper>
                   <UserImg source={item.userImg} />
@@ -116,22 +129,35 @@ const ChatTabScreen: React.FC<MessagesScreenProps> = ({ navigation }) => {
             </Card>
           )}
         />
-      </Container>
-    </GestureHandlerRootView>
+        </Container>
+
+        <BottomSheetModalProvider>
+        <BottomSheetModal
+            ref={bottomSheetModalRef}
+            onChange={handleSheetChanges}
+          >
+            <BottomSheetView style={styles.contentContainer}>
+              <Text>Awesome 🎉</Text>
+            </BottomSheetView>
+        </BottomSheetModal>
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'grey',
-  },
+  // container: {
+  //   flex: 1,
+  //   padding: 24,
+  //   justifyContent: 'center',
+  //   backgroundColor: 'grey',
+  // },
   contentContainer: {
     flex: 1,
-    padding: 36,
     alignItems: 'center',
+    minHeight: 400,
   },
 });
-
 
 export default ChatTabScreen;
