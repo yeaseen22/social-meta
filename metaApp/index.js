40c8d95505f01d7e22d7b9a1914e8645c3bf4c1d
asadanik <s.engineer63@gmail.com>
2025-02-02 20:13:47 +0600

    fix: mobile app issue

--- metaApp/index.js
+++ metaApp/index.js
@@ -2,9 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 
 // Suppress the specific warning
