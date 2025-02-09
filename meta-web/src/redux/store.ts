f716ad18630ac89a12e47dcdbb37211b79b94b86
Yeaseen Arafat <yeaseenarafat@Yeaseens-MacBook-Pro.local>
2025-02-09 14:48:03 +0600

    adding comment3

--- meta-web/src/redux/store.ts
+++ meta-web/src/redux/store.ts
@@ -7,7 +7,7 @@ import { persistReducer, persistStore } from 'redux-persist';
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['auth'], // only persist auth state
+  whitelist: ['auth'],
 };
 
 const rootReducer = combineReducers({
